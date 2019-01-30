e175f03545259a115ac4a3dcdd5fcdfb17004c38
guyaqi5858 <guyaqi5858@outlook.com>
2019-01-30 21:25:51 +0800

    m

---
+++ sleep.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+#ifdef WIN32
+#include <Windows.h>
+#else
+#include <unistd.h>
+#endif
+
+void sleep();
+
+int main() {
+	char str[] = "Hello, world!";
+	for (int i = 0; i < sizeof(str)-1; i++) {
+		putchar(str[i]);
+		sleep();
+	}
+	return 0;
+}
+
+void sleep() {
+#ifdef WIN32
+	Sleep(600);
+#else
+	usleep(600000);
+#endif
+}
